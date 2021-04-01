5b814a76d97d70025bb6d6a0301a9d72f28d587d
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2021-04-01 22:12:27 +0300

    Users now have access to edit/delete buttons on their own posts.

--- Final Project/app-project/src/components/BlogPostDetails/BlogPostDetails.js
+++ Final Project/app-project/src/components/BlogPostDetails/BlogPostDetails.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import style from './BlogPostDetails.module.css';
 import blogPostService from '../../services/blogPostService';
+import authService from '../../services/authService';
 
 class BlogPostDetails extends Component {
     constructor(props) {
@@ -8,12 +9,13 @@ class BlogPostDetails extends Component {
 
         this.state = {
             blogPost: null,
+            userData: authService.getData(),
         }
     }
 
     componentDidMount() {
         const { blogPostId } = this.props.match.params;
-        
+
         blogPostService.getOne(blogPostId)
             .then(blogPost => {
                 this.setState({ blogPost });
@@ -23,18 +25,36 @@ class BlogPostDetails extends Component {
             });
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.userData.isAuthenticated !== authService.getData().isAuthenticated) {
+            this.setState({ userData: authService.getData() });
+        }  
+        return;
+    }
+
     render() {
-        let blogPost = {...this.state.blogPost}
-        
+        let blogPost = { ...this.state.blogPost }
+        let isAuthenticated = this.state.userData.isAuthenticated;
+        let isCreator = this.state.userData.email == blogPost.creator;
+
         return (
-            
+
             <main className={style.main}>
                 <div className={style.blogPostDetails}>
                     <h2>{blogPost.title}</h2>
                     <img src={blogPost.imageUrl} />
                     <p>{blogPost.content}</p>
-                    <button>Delete</button>
-                    <button>Edit</button>
+
+                    {
+                        isAuthenticated
+                        && isCreator
+                        && (
+                            <div>
+                                <button>Delete</button>
+                                <button>Edit</button>
+                            </div>
+                        )}
+
                 </div>
             </main>
         );
